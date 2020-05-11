04575d13b899605747a9d5c68b987f5ad70ba8be
Rm4n <arm1377arman@gmail.com>
2020-05-11 10:55:54 +0430

    Updates

---
+++ precache-manifest.17da5d2d5f5281b1feff47ea5f5de68c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5670a603f54512c34295c0c6cb76e11e",
+    "url": "/COVID19/index.html"
+  },
+  {
+    "revision": "29bbc34016409392b768",
+    "url": "/COVID19/static/css/main.3696c605.chunk.css"
+  },
+  {
+    "revision": "4bd051426ad1fe208b77",
+    "url": "/COVID19/static/js/2.ca0c3277.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19/static/js/2.ca0c3277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29bbc34016409392b768",
+    "url": "/COVID19/static/js/main.ee71ecf2.chunk.js"
+  },
+  {
+    "revision": "91c80a388bac44ec3666",
+    "url": "/COVID19/static/js/runtime-main.c702c315.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19/static/media/image.d7265326.png"
+  }
+]);
